9946298eeb9feb794c8f954905368080c456b180
qmenchaca <maximomenchaca@gmail.com>
2019-05-11 14:42:32 -0700

    Adding Admin to update Cat information
    Cleaning up code a bit as well - still learning JavaScript/jQuery!

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(function(){
 
   var model = {
+    currentCat: null,
     catList: [
               {name:"Dave", counter:0, path:"Dave.jpg"},
               {name:"Rocky", counter:0, path:"Rocky.jpg"},
@@ -12,14 +13,24 @@ $(function(){
 
     incrementCount: function(cat) {
       cat.counter += 1;
+    },
+
+    setName: function(cat,str) {
+      cat.name = str;
+    },
+
+    prettyName: function(cat) {
+      return cat.name.replace(/ /g,'_');
     }
+
   };
 
   var octopus = {
     init: function() {
       for (var i = 0; i < model.catList.length; i++) {
-        var this_id = 'button_' + i;
-        $cat_button = $('<li id=${this_id}><a href="#">' + model.catList[i].name + '</a></li>');
+        var this_name = model.catList[i].name
+        var link_id = 'link_' +  model.prettyName(model.catList[i]);
+        $cat_button = $('<li ><a href="#" id=' + link_id + '>' + this_name + '</a></li>');
   
   
         $($cat_button).click({cat: model.catList[i]}, function(event) {
@@ -28,13 +39,73 @@ $(function(){
         $(".cat-list").append($cat_button);
       };
       view.init();
+      octopus.setCat(model.catList[0]);
+
+      octopus.hideAdmin();
+      //octopus.setAdminButton();
+      //octopus.updateCatDeets();
     },
 
     increment_cat: function(cat) {
       model.incrementCount(cat);
-      view.updateCat(cat);
-    }
+      view.display_cat(cat);
+    },
 
+    setCat: function(cat) {
+      model.currentCat = cat;
+    },
+
+    getCat: function() {
+      return model.currentCat;
+    },
+
+    showAdmin: function() {
+      $('.admin-section')[0].style.display = "block"
+    },
+
+    hideAdmin: function() {
+      $('.admin-section')[0].style.display = "none"
+    },
+
+    setAdminButton: function() {
+        curr_cat = octopus.getCat();
+        $('.admin-button').click({cat: curr_cat}, function(event) {
+          var cat_name= $('.name-form');
+          cat_name.val(event.data.cat.name);
+
+          var cat_path= $('.url-form');
+          cat_path.val(event.data.cat.path);
+
+          var cat_count= $('.count-form');
+          cat_count.val(event.data.cat.counter);
+
+          octopus.showAdmin();
+        });
+    },
+
+    updateCatDeets: function() {
+        $('.update-button').click({cat: curr_cat}, function(event) {
+          curr_cat = octopus.getCat();
+
+          old_nice_name = model.prettyName(curr_cat);
+          var cat_name= $('.name-form');
+          model.setName(curr_cat,cat_name.val());
+
+          var cat_path= $('.url-form');
+          curr_cat.path = cat_path.val();
+
+          var cat_count= $('.count-form');
+          curr_cat.counter = parseInt(cat_count.val());
+
+          octopus.hideAdmin();
+          var this_id = 'link_' + old_nice_name;
+          $('#' + this_id).text(cat_name.val());
+          $('#' + this_id).attr('id','link_' + cat_name.val());
+
+          updated_cat = octopus.getCat();
+          view.display_cat(curr_cat);
+        });
+    }
 
   };
 
@@ -50,23 +121,21 @@ $(function(){
       body.getElementsByClassName("pic")[0].src= 'pictures/' + cat.path;
       body.getElementsByClassName("cl-text")[0].textContent = "You have clicked on " + cat.name + " " + cat.counter + " times.";
 
-      $("#content").data("cat_obj", cat);
+      octopus.setCat(cat);
+      octopus.setAdminButton();
+      octopus.updateCatDeets();
     
-    },
-
-    updateCat: function(cat) {
-      var body = document.getElementById("content");
-      body.getElementsByClassName("cl-text")[0].textContent = "You have clicked on " + cat.name + " " + cat.counter + " times.";
     }
   };
+
 
   octopus.init();
 
 
+
   $('#content').on('click', '.pic', function(e) {
   
-      var $cat_obj = $(this).parent().data("cat_obj");
-  
+      $cat_obj = octopus.getCat();
       octopus.increment_cat($cat_obj);
   
   });
